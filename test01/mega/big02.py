0d342d1e26fb49ceb0622eea32ff0da3ec69801d
csangh94 <60869757+csangh94@users.noreply.github.com>
2020-02-11 18:17:27 +0900

    Add files via upload

---
+++ test01/mega/big02.py
@@ -0,0 +1,5 @@
+print("이상혁")
+print("김포")
+print("27세")
+print("헬스")
+print("졸림")
---
+++ test01/mega/big04.py
@@ -0,0 +1,8 @@
+# 기본 입력 처리
+# name, age 는 ram의 저장곤간
+name = input("이름 입력: ")
+age = input("나이 입력: ")
+print(name, "님 환영합니다.")
+print("당신의 나이는", age, "세")
+
+
